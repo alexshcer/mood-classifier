acd42c7957e01cc7dfbd577e0b001eed2f42159c
alexshcer <alexmerser591@gmail.com>
2024-11-14 05:52:36 +0530

    v3

--- preload.js
+++ preload.js
@@ -3,4 +3,4 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     downloadYouTubeAudio: (url) => ipcRenderer.invoke('download-youtube-audio', url),
     readAudioFile: (filePath) => ipcRenderer.invoke('read-audio-file', filePath)
-}); 
+});
